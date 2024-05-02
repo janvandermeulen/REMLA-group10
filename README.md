d890fd6f6537f1168509bbc0ff4c257c3f6c6274
Jan <j2000.vdm@gmail.com>
2024-05-02 14:03:13 +0200

    Added versioning with poetry

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ In this assignment we will be transferring a small kaggle model to a professiona
 - dvc for data version control and machine learning reproducibility.
 - git for version control.
 - poetry for dependency management.
+
+### How to run
+To run this codee you need to have poetry installed. You can install the packages by running the following command:
+- ```poetry install```
--- phishing-detection/phishing_detection/preprocess.py
+++ phishing-detection/phishing_detection/preprocess.py
@@ -1,3 +1,14 @@
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing.text import Tokenizer
+import numpy as np
+import pandas as pd
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, Conv1D, MaxPooling1D, Flatten
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix
 # Stage 1: Preprocess the data
 # Load in the dataset
 # TODO: fix the sources
